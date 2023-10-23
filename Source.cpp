5588deff7c498cc265856bb21dd41809f1852f7a
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-10-23 22:03:03 +0300

    1

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,9 @@
 #include <iostream>
+int main() {
+    std::cout << "I love my cat," << std::endl;
+    std::cout << "It's warm and fat" << std::endl;
+    std::cout << "My cat is grey" << std::endl;
+    std::cout << "It likes to play." << std::endl;
+
+    return 0;
+}
